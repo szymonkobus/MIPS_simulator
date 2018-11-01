668ccc5e159b61bc32afe3995e5a99d280122d44
ak9717 <andrzej.kowalewski17@imperial.ac.uk>
2018-11-01 20:40:36 +0000

    instructions protorypes

--- cpu.hpp
+++ cpu.hpp
@@ -25,9 +25,33 @@ private:
   void execute_i(const instruction& inst);
   void execute_j(const instruction& inst);
 
-  void SLL(const instruction& inst);
-  void SRL(const instruction& inst);
-  void ADDU(const instruction& inst);
+  void cpu::SLL(const instruction& inst);
+  void cpu::SRL(const instruction& inst);
+  void cpu::SRA(const instruction& inst);
+  void cpu::JR(const instruction& inst);
+  void cpu::MFHI(const instruction& inst);
+  void cpu::MFLO(const instruction& inst);
+  void cpu::MULT(const instruction& inst);
+  void cpu::MULTU(const instruction& inst);
+  void cpu::DIV(const instruction& inst);
+  void cpu::DIVU(const instruction& inst);
+  void cpu::ADD(const instruction& inst);
+  void cpu::ADDU(const instruction& inst);
+  void cpu::SUB(const instruction& inst);
+  void cpu::SUBU(const instruction& inst);
+  void cpu::AND(const instruction& inst);
+  void cpu::OR(const instruction& inst);
+  void cpu::XOR(const instruction& inst);
+  void cpu::NOR(const instruction& inst);
+  void cpu::NOR(const instruction& inst);
+  void cpu::NOR(const instruction& inst);
+  void cpu::NOR(const instruction& inst);
+  void cpu::NOR(const instruction& inst);
+  void cpu::NOR(const instruction& inst);
+  void cpu::NOR(const instruction& inst);
+
+
+
 };
 
 #endif
