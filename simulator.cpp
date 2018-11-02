dbee95b7727881c553d85efeafa5af06e3d657b3
szymonkobus <36537361+szymonkobus@users.noreply.github.com>
2018-11-02 00:31:48 +0000

    resloving merges v1

--- simulator.cpp
+++ simulator.cpp
@@ -10,19 +10,18 @@ int main(int argc, char const *argv[]) {
   //std::cout << "m2" << '\n';
   //m2.print_mem();
 
-<<<<<<< HEAD
   cpu c("bin_tst2.bin");
 
   //std::cout << "cpu mem: " << "\n";
   //c.m.print_mem();
 
-=======
   cpu c("bin_tst3.bin");
   std::cout << "cpu mem: " << "\n";
   c.m.print_mem();
   c.test_fill();
   c.reg_print();
->>>>>>> a3f9539582abd8289daae68f61739c4a322774f9
+
+
   c.run();
 
   return 0;
