2f04e03dcb3404f9b5e86ebc20fc7c45cfa9a0c6
szymonkobus <36537361+szymonkobus@users.noreply.github.com>
2018-11-04 15:12:06 +0000

    restore main

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -4,11 +4,14 @@
 #include "cpu.hpp"
 
 int main(int argc, char const *argv[]) {
+  String name_bin = "MIPS_bin/bin_tst10.bin";
 
-  cpu c(name_bin);
+  if(argc == 2) name_bin = argv[1];
+  cpu c(name_bin)
 
   std::cerr << "cpu mem: " << "\n";
   c.m.print_mem();
+  c.test_fill();
   std::cerr << "cpu registers initial"<<std::endl;
   c.reg_print(1);
 
