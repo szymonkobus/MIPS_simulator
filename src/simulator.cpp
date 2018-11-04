2726ebaf9c3b908b9c66bbf622791b456a43c040
ak9717 <andrzej.kowalewski17@imperial.ac.uk>
2018-11-04 14:36:37 +0000

    merge

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -4,12 +4,7 @@
 #include "cpu.hpp"
 
 int main(int argc, char const *argv[]) {
-<<<<<<< HEAD:simulator.cpp
-
-  cpu c("./bin/bin_tst11.bin");
-=======
   cpu c("MIPS_bin/bin_tst10.bin");
->>>>>>> 96f0c1d0a4287710346e1535752328d72eaafc3c:src/simulator.cpp
 
   std::cerr << "cpu mem: " << "\n";
   c.m.print_mem();
