a9c0211c5ca17e706bcb0070440109ddc9327754
szymonkobus <36537361+szymonkobus@users.noreply.github.com>
2018-11-06 23:42:24 +0000

    basis for benchmarks

--- testbench.py
+++ testbench.py
@@ -4,6 +4,18 @@ import subprocess as sps
 def file_exists(file):
     return os.path.exists(file)
 
+def create_bin(file, TestId, p_parser, p_tests_binary):
+    if(file_exists(p_parser)):
+        par = sps.Popen([p_parser],stdin=sps.PIPE, stderr=sps.PIPE, stdout=sps.PIPE)
+        code = ""
+        next_line = file.readline()
+        next_line = file.readline()
+        while(next_line != None and next_line != ""):
+            code = code + next_line;
+            next_line = file.readline()
+        code = code + "exit\n" + p_tests_binary + TestId + ".bin\n"
+        par.communicate(input = code.encode())
+
 def print_file(TestId, Instruction, Author, Exit, Message):
     print("TestId: " + TestId)
     print("Instruction: " + Instruction)
@@ -13,15 +25,15 @@ def print_file(TestId, Instruction, Author, Exit, Message):
 
 p_parser = "parser/bin/parser"
 p_tests = "benchmark_src/tests/"
-p_tests_code = "benchmark_src/tst_bench_txt/"
+p_tests_code = "benchmark_src/tst_bench_code/"
 p_tests_binary = "benchmark_src/tst_bench_bin/"
 
 simulator = "bin/mips_simulator"
 
 files = os.listdir(p_tests)
 
-for file in files:
-    file = open(p_tests + file)
+for file_name in files:
+    file = open(p_tests + file_name)
 
     TestId = file.readline()[8:-1]
     Instruction = file.readline()[13:-1]
@@ -33,13 +45,11 @@ for file in files:
 
     #add some protection later
     if(not file_exists(p_tests_binary + TestId + ".bin")):
-        sps.call([p_parser, p_tests_code + TestId + ".txt", p_tests_binary + TestId + ".bin"], stderr=sps.PIPE, stdout=sps.PIPE)
+        create_bin(file, TestId, p_parser, p_tests_binary)
 
-
-    exit = sps.call([simulator, p_tests_binary + TestId + ".bin"], stderr=sps.PIPE)
-
-    Status = "Pass" if (int(Expected_Exit) == exit) else "Fail"
-
-    # TestId , Instruction , Status , Author [, Message]
-    print(TestId + " , " + Instruction + " , " + Status + " , " + Author + " , " + Message)
+    if(file_exists(p_tests_binary + TestId + ".bin")):
+        exit = sps.call([simulator, p_tests_binary + TestId + ".bin"], stderr=sps.PIPE)
+        Status = "Pass" if (int(Expected_Exit) == exit) else "Fail"
+        # TestId , Instruction , Status , Author [, Message]
+        print(TestId + " , " + Instruction + " , " + Status + " , " + Author + " , " + Message)
     file.close()
