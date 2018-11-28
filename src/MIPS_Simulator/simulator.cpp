d6c58d021e4f3040768d18065fb93b7d76e4b578
ak9717 <andrzej.kowalewski17@imperial.ac.uk>
2018-11-28 09:49:12 +0000

    cerr instead of cout

--- src/MIPS_Simulator/simulator.cpp
+++ src/MIPS_Simulator/simulator.cpp
@@ -8,7 +8,7 @@ int main(int argc, char const *argv[]) {
 
   if(argc == 2) name_bin = argv[1];
   else{
-    std::cout<<"binary not found"<<std::endl;
+    std::cerr<<"binary not found"<<std::endl;
     exit(-21);
   }
 
